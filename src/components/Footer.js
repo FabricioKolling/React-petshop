e5080c77c090103258326536d93d12c899edaa3b
Fabricio Kolling <162700727+FabricioKolling@users.noreply.github.com>
2025-06-06 09:27:21 -0300

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,40 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { FaPhone, FaWhatsapp, FaMapMarkerAlt, FaFacebookF, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 
-function Footer(props) {
+function Footer() {
   return (
-    <Container>
-      <Row>
-        <Col className='formulario'>
-            <h1>TITULO</h1>
-
-            <div className='forms'>
-
-                <input type='text' id="nome" value={ props.nomeInput } placeholder='Nome' />
-                <input type='email' id="email" value={ props.emailInput } placeholder='Email' />
-                <input type='text' id="nome" className='mensagemForms' value={ props.mensagemInput } placeholder='Mensagem' />
+    <div className="footer-wrapper">
+      <Container>
+        <Row>
+          <h5 className='tituloFooter'>Fale Conosco</h5>
+          <Col md={7} className='formularioCol'>
+            <form className='forms'>
+              <input type='text' placeholder='Nome' />
+              <input type='email' placeholder='E-mail' />
+              <textarea className='mensagemForms' placeholder='Mensagem'></textarea>
+            </form>
+            <div className="button-wrapper">
+              <button id='enviarButton' className='botaoAgora'>Enviar Agora</button>
             </div>
-            
-            <button id='enviar' className='botaoEnviar'>Enviar Agora</button>
-        </Col>
-        <Col>
-        
-        </Col>
-      </Row>  
-    </Container>
+          </Col>
+
+          <Col md={5} className="contact-info-col">
+            <div className="contact-details">
+              <p><FaPhone /> (48) 3222 0098</p>
+              <p><FaWhatsapp /> (48) 99383 0928</p>
+              <p><FaMapMarkerAlt /> Rua Marechal Deodoro, 55 - Centro - Florianópolis/SC</p>
+            </div>
+            <div className="social-icons">
+              <a href="#!" aria-label="Facebook"><FaFacebookF /></a>
+              <a href="#!" aria-label="Instagram"><FaInstagram /></a>
+              <a href="#!" aria-label="LinkedIn"><FaLinkedinIn /></a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
--- src/pages/Adestramento.js
+++ src/pages/Adestramento.js
@@ -1,9 +1,30 @@
 import React from "react";
 
+import Rotulo from "../components/Rotulo";
+import StaticBanner from "../components/StaticBanner";
+import ContentPhotos from "../components/ContentPhotos";
+import FAQ from "../components/FAQ";
+
+import bannerImagem from "../imagens/adestramento/banner-adestramento.jpg";
+import foto1 from '../imagens/adestramento/adestramento1.jpg';
+import foto2 from '../imagens/adestramento/adestramento2.jpg';
+import foto3 from '../imagens/adestramento/adestramento3.jpg';
+import foto4 from '../imagens/adestramento/adestramento4.jpg';
+import foto5 from '../imagens/adestramento/adestramento5.jpg';
+
 const Adestramento = () => {
 	return (
 		<div>
-			<h1>Adestramento</h1>
+			<Rotulo textRotulo='Adestramento' />
+			<StaticBanner bannerAdestramento={bannerImagem} />
+            <ContentPhotos 
+                img1={foto1}
+                img2={foto2}
+                img3={foto3}
+                img4={foto4}
+                img5={foto5}
+            />
+            <FAQ />
 		</div>
 	);
 };
